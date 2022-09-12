7b28ddcfb65cd425a2bab958310d2b517dc8a674
ayaka-kanou <77620409+ayaka-kanou@users.noreply.github.com>
2022-09-12 21:26:04 +0900

    Update and rename js to header.js

---
+++ header.js
@@ -0,0 +1,33 @@
+document.write(
+    '<header>'+
+        '<div class="sub_header_inner">'+
+                '<div class="header_wrap transition_trg">'+
+                    '<h1 class="logo" style="display: block;">'+
+                        'Web Desgin'+
+                    '</h1>'+
+                '</div>'+
+            '</div>'+
+        
+            '<!-- PC Menu -->'+
+            '<nav>'+            
+                '<ul class="menu">'+
+                    '<li class="portfolio">'+
+                        '<a href="#portfolio">'+
+                            'Portfolio'+
+                        '</a>'+
+                    '</li>'+
+                    '<li class="profile">'+
+                        '<a href="#profile">'+
+                            'Profile'+
+                        '</a>'+
+                    '</li>'+
+                    '<li class="contact">'+
+                        '<a href="#contact">'+
+                            'Contact'+
+                        '</a>'+
+                    '</li>'+
+                '</ul>'+
+            '</nav>'+
+        '</div>'+
+    '</header>'
+);
